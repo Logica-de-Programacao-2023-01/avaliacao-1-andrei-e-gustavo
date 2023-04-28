ede778edeada9fd0670430e152f50cc2c6815286
andreibromaskevis <126252917+andreibromaskevis@users.noreply.github.com>
2023-04-28 10:00:38 -0300

    Update q3.go

--- q3/q3.go
+++ q3/q3.go
@@ -15,13 +15,12 @@ import "fmt"
 //Se M ou N forem iguais ou menores que 0, a função deve retornar um erro.
 
 
-
 func DominoPieces(m, n int) (int, error) {
 if m <= 0 || n <= 0 {
-		return 0, fmt.Errorf("invalido")
+return 0, fmt.Errorf("invalido")
 } else {
-		NumeroDePecas := (m * n) / 2
-		return NumeroDePecas, nil
+	NumeroDePecas := (m * n) / 2
+	return NumeroDePecas, nil
 }
 	return 0, nil
 }
